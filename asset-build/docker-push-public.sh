ac19d790213626d8fe6ff9de7278660a70e3462c
Shrey Ajmera <59667774+sajmera-pensando@users.noreply.github.com>
2024-10-30 13:57:28 -0700

    Enhance the public push script to take public and private tags. (#200)

--- asset-build/docker-push-public.sh
+++ asset-build/docker-push-public.sh
@@ -6,9 +6,12 @@ then
       exit 0
 fi
 
-docker rmi registry.test.pensando.io:5000/amd-gpu-operator:latest
-docker pull registry.test.pensando.io:5000/amd-gpu-operator:latest
-docker tag registry.test.pensando.io:5000/amd-gpu-operator:latest amdpsdo/gpu-operator:latest
+PUBLIC_TAG=${PUBLIC_TAG:-latest}
+PRIVATE_TAG=${PRIVATE_TAG:-latest}
+
+docker rmi registry.test.pensando.io:5000/amd-gpu-operator:$PRIVATE_TAG
+docker pull registry.test.pensando.io:5000/amd-gpu-operator:$PRIVATE_TAG
+docker tag registry.test.pensando.io:5000/amd-gpu-operator:$PRIVATE_TAG amdpsdo/gpu-operator:$PUBLIC_TAG
 
 docker login --username=shreyajmeraamd --password-stdin <<< $DOCKERHUB_TOKEN
-docker push amdpsdo/gpu-operator:latest
+docker push amdpsdo/gpu-operator:$PUBLIC_TAG
