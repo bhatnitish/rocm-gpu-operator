bfdef976ea53d76859e50c711245fbeae4d70cdc
Farshad Ghodsian <47931571+farshadghodsian@users.noreply.github.com>
2025-03-31 18:17:33 -0400

    Updated ReadTheDocs conf to support Copy Code Block Button (#524)
    * Updated ReadTheDocs conf to support Copy Code Block Button
    
    * Removed old generic theme variables in docs conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,26 +1,27 @@
 """Configuration file for the Sphinx documentation builder."""
+import os
 
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "instinct.docs.amd.com")
+html_context = {}
+if os.environ.get("READTHEDOCS", "") == "True":
+    html_context["READTHEDOCS"] = True
 external_projects_local_file = "projects.yaml"
 external_projects_remote_repository = ""
 external_projects = ["amd-gpu-operator"]
 external_projects_current_project = "amd-gpu-operator"
 
-project = "AMD Instinct Hub"
-version = "1.1.0"
+project = "AMD GPU Operator"
+version = "1.2.0"
 release = version
 html_title = f"{project} {version}"
 author = "Advanced Micro Devices, Inc."
-copyright = "Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved."
+copyright = "Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved."
 
 # Required settings
 html_theme = "rocm_docs_theme"
 html_theme_options = {
-    "flavor": "generic",
-    "header_title": "AMD GPU Operator",
-    "nav_secondary_items": {
-        "GitHub": "https://github.com/rocm/gpu-operator",
-        "Documentation": "https://dcgpu.docs.amd.com"
-    },
+    "flavor": "instinct",
+    "link_main_doc": True,
     # Add any additional theme options here
 }
 extensions = ["rocm_docs"]
